39ec7f4ae5a0a7e4af9b2b9fdadf1bb588a0d58d
Ghassan <g4ss4n@gmail.com>
2019-10-02 18:44:19 +0300

    positions fixed

--- EasyBusiness/src/components/Input/input.js
+++ EasyBusiness/src/components/Input/input.js
@@ -5,6 +5,7 @@ import { OmitProps } from "antd/lib/transfer/renderListBody";
 const Input = props => {
   return (
     <input
+      className="inputt"
       type={props.type}
       value={props.Input}
       onInput={e => props.setInput(e.target.value)}
